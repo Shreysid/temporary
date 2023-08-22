e7546b9c55a6339fb829ac85bb0cb816901c8098
Sanjay S <sanu2336@gmail.com>
2023-08-22 10:11:26 +0000

    Saving Hero section changes from codespace 22/8

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,18 +1,30 @@
+import Star from "./assets/Star";
 import hero from "./assets/hero.png";
 const Hero = () => {
   return (
     <div className="w-full m-auto flex flex-col md:flex-row lg:flex-row h-[80vh] w-full px-12 md:px-8 lg:px-4">
       <div className="flex flex-col md:w-1/2 justify-center text-center items-center md:items-start md:text-left lg:text-left">
-        <h1 className="text-orange font-bold text-5xl md:text-6xl lg:text-7xl leading-none md:leading-tight">
+        <h1 className="text-orange font-bold text-3xl md:text-5xl lg:text-7xl md:w-3/4">
           {" "}
           <span className="text-navy">Supercharge</span> your online business{" "}
         </h1>
-        <p className="text-gray pt-4 text-2xl">
+        <h2 className="text-gray pt-4 text-xl md:text-2xl">
           Selling online is as simple as sharing content.
-        </p>
+        </h2>
         <button className="px-6 py-2 bg-orange w-36 my-4 text-white rounded-full">
           Explore
         </button>
+        <div className="flex flex-col md:flex-row">
+          <div className="flex">
+            <div className="h-10 w-10 bg-red-400 rounded-full"></div>
+            <div className="h-10 w-10 bg-blue-400 rounded-full -translate-x-4"></div>
+            <div className="h-10 w-10 bg-yellow-400 rounded-full -translate-x-8"></div>
+          </div>
+          <div className="flex flex-col items-start md:-translate-x-4">
+            <h3 className="font-bold text-sm">Customer reviews</h3>
+            <p className="flex items-center"><span className="flex items-center"><Star/> 4.8</span> (5k reviews)</p>
+          </div>
+        </div>
       </div>
       <div className="md:w-1/2 mx-auto flex justify-center items-center">
         <img className="w-full" src={hero} alt="hero image" />
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -2,10 +2,10 @@ import Boost from "./assets/Boost.tsx";
 
 function Nav() {
   return (
-    <nav className="w-full mx-4">
+    <nav className="w-full h-20 mx-4 flex">
       <div className="flex items-center">
         <Boost />
-        <h1 className="font-poppins font-light text-3xl p-2 text-orange">
+        <h1 className="font-poppins font-light text-2xl md:text-3xl p-2 text-orange">
           BOOST
         </h1>
       </div>
